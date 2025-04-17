f7334614cccdc6446d93776ae85d4bdb19d5be18
Volkan Kök <volkankok@gmail.com>
2025-04-17 16:06:24 +0300

    main

--- frontend/src/pages/Ana.jsx
+++ frontend/src/pages/Ana.jsx
@@ -7,15 +7,15 @@ export default function Ana() {
       <div className="container mx-auto px-1 lg:px-20 flex flex-col lg:flex-row items-center">
         
         {/* Sol: Metin Alanı */}
-        <div className="lg:w-1/2 text-center lg:text-left max-w-6xl ">
+        <div className="lg:w-7/10 text-center lg:text-left max-w-6xl ">
           <div className="mb-4">
-            <span className="inline-block bg-fuchsia-400 text-white px-4 py-2 rounded-full text-md font-semibold mt-5 ml-1">
-              ✌️ Merhaba! Ben Volkan
+            <span className="inline-block bg-fuchsia-400 text-white px-4 py-2 rounded-full text-md font-semibold mt-5 ml-1 text-5xl">
+            👋 Merhaba! Ben &lt;Volkan/&gt;
             </span>
             
           </div>
           <h1 className="text-4xl lg:text-6xl font-bold leading-tight mb-4">
-            <span className="text-purple-400">Full Stack Developer.</span > <span className="text-blue-200"> Startup Oluşumlara Kendi Ürünlerini</span> <span className="text-indigo-400">Yayınlamada </span> <span className="text-blue-200">ve</span> <span className="text-indigo-400">Büyütmede </span> <span className="text-blue-200">yardımcı oluyorum </span>
+            <span className="text-purple-400">Full Stack Developer.</span > <span className="text-blue-200"> Startup Oluşumlara Kendi Ürünlerini</span> <span className="text-indigo-400">Yayınlamada </span> <span className="text-blue-200">ve</span> <span className="text-indigo-400">Büyütmede </span><br></br> <span className="text-blue-200">yardımcı oluyorum </span>
           </h1>
           <p className="text-blue-200 text-lg leading-relaxed pl-4 mb-4">
             4+ yıllık profesyonel deneyimimle çeşitli programlama araçları kullanarak müşterilere kaliteli sonuçlar sunuyorum. Frontend, backend, yazılım testi ve web3 geliştirme konularında uzman bilgiye sahibim.
@@ -23,7 +23,7 @@ export default function Ana() {
         </div>
 
         {/* Sağ: Görsel Alanı */}
-        <div className="lg:w-1/2 mt-10 lg:mt-0 flex lg:justify-end">
+        <div className="lg:w-3/10 mt-10 lg:mt-0 flex lg:justify-end">
           <img
             src={kafa} // Görsel yolunu buraya ekleyin
             alt="Frontend Engineer Illustration"
